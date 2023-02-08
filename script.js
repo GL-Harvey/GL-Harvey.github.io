4fd128e0dcecafe8aa0c3ea116195477d8687fbd
Tim Wünsch <44634581+Harvezz@users.noreply.github.com>
2023-02-08 12:34:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var UserStatus;
 })(UserStatus || (UserStatus = {}));
 var Default;
 (function (Default) {
-    Default["PIN"] = "6969";
+    Default["PIN"] = "1234";
 })(Default || (Default = {}));
 var WeatherType;
 (function (WeatherType) {
@@ -189,7 +189,7 @@ const Pin = () => {
             React.createElement(PinDigit, { focused: pin.length === 2, value: pin[2] }),
             React.createElement(PinDigit, { focused: pin.length === 3, value: pin[3] })),
         React.createElement("h3", { id: "app-pin-label" },
-            "Enter PIN (6969) ",
+            "Enter PIN (1234) ",
             getErrorText(),
             " ",
             getCancelText())));
@@ -302,13 +302,13 @@ const Tools = () => {
                 id: 2,
                 image: "https://images.unsplash.com/photo-1587145820266-a5951ee6f620?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FsY3VsYXRvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
                 label: "Calc",
-                name: "Mathio"
+                name: "Maths"
             }, {
                 icon: "fa-solid fa-piggy-bank",
                 id: 3,
                 image: "https://images.unsplash.com/photo-1579621970588-a35d0e7ab9b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmFua3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
                 label: "Bank",
-                name: "Cashy"
+                name: "Money"
             }, {
                 icon: "fa-solid fa-plane",
                 id: 4,
@@ -320,13 +320,13 @@ const Tools = () => {
                 id: 5,
                 image: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmlkZW8lMjBnYW1lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
                 label: "Games",
-                name: "Gamey"
+                name: "Gaming"
             }, {
                 icon: "fa-solid fa-video",
                 id: 6,
                 image: "https://images.unsplash.com/photo-1578022761797-b8636ac1773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHZpZGVvJTIwY2hhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
                 label: "Video Chat",
-                name: "Chatty"
+                name: "Chatting"
             }].map((tool) => {
             const styles = {
                 backgroundImage: `url(${tool.image})`
